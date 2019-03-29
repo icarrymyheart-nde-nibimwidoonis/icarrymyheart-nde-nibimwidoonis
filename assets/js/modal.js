2ab6f4559e7d59a55aba47a1b7c5e8da54f8a744
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2019-03-28 21:12:47 -0400

    optimize images and clean up code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -10,15 +10,12 @@ img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
 }
-
 imgb.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
 }
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on <span> (x), close the modal
 modal.onclick = function() {
   modal.style.display = "none";
